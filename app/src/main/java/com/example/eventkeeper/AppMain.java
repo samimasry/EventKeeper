a31d7a843e6ee019a611ae8d20cebf051fac105f
Sami Masry <42784843+samimasry@users.noreply.github.com>
2019-04-14 11:48:42 -0400

    Create AppMain.java

---
+++ app/src/main/java/com/example/eventkeeper/AppMain.java
@@ -0,0 +1,13 @@
+package com.example.eventkeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_main);
+    }
+}
